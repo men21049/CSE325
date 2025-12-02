40924f6e48db01cf0b0612197ff45b2f96f2e2cd
iezll1106 <llizardo@byupathway.edu>
2025-12-02 14:58:06 +0800

    Edit Program.cs, NavMenu, Add Document.razor, etc

---
+++ DocumentManagementSystem/Models/DocumentModel.cs
@@ -0,0 +1,9 @@
+namespace DocumentManagementSystem.Model
+{
+    public class DocumentModel
+    {
+        public string FileName { get; set; }
+        public string Office { get; set; }
+        public DateTime DateUploaded { get; set; }
+    }
+}
